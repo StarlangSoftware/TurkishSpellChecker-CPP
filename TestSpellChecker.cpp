a136aa4bac6681c97818a1ad45fd32ceab4049c7
olcaytaner <olcaytaner@gmail.com>
2019-03-10 22:46:46 +0300

    Updated spellchecker repository translation

--- TestSpellChecker.cpp
+++ TestSpellChecker.cpp
@@ -10,6 +10,6 @@ int main(){
     FsmMorphologicalAnalyzer fsm;
     fsm = FsmMorphologicalAnalyzer();
     SimpleSpellChecker spellChecker = SimpleSpellChecker(fsm);
-    Sentence* s1 = new Sentence("lokilde güzilce gezdık .");
+    Sentence* s1 = new Sentence("evdı gezdık .");
     Sentence* s2 = spellChecker.spellCheck(s1);
 }
