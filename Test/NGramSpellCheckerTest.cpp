c229fe5167ffa3fbbb5cfa8e4dae5d4892601247
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-09-25 23:48:59 +0300

    Updated some test cases.

--- Test/NGramSpellCheckerTest.cpp
+++ Test/NGramSpellCheckerTest.cpp
@@ -22,12 +22,11 @@ TEST_CASE("NGramSpellCheckerTest-testSpellCheck") {
                            new Sentence("yılın son ayında 10 gazeteci gözaltına alındı"),
                            new Sentence("iki pilotun kullandığı uçakta bir hostes görev alıyor"),
                            new Sentence("son derece kısıtlı kelimeler çerçevesinde kendilerini uzun cümlelerle ifade edebiliyorlar"),
-                           new Sentence("kedi köpek"),
                            new Sentence("minibüs durağı"),
                            new Sentence("noter belgesi"),
                            new Sentence("")};
     Sentence* modified[] = {new Sentence("demokratik cumhüriyet rn kımetli varlıgımızdır"),
-                           new Sentence("bu tblodaki değerlğr zedelenmeyecüktir"),
+                           new Sentence("bu tblodaki değerler zedelenmeyecektir"),
                            new Sentence("milliyet'in geeneksel yılin spoşcusu ankşti 43. yeşını doldürdu"),
                            new Sentence("demokrasinin icşdı buf ayrmıı bulandürdı"),
                            new Sentence("dışişleri mütseşarı Öymen'in 1997'nin iljk aylğrında Bağdat'a gitmesi öngörülüyor"),
@@ -36,12 +35,11 @@ TEST_CASE("NGramSpellCheckerTest-testSpellCheck") {
                            new Sentence("yılın sno ayında 10 gazteci gözlatına alündı"),
                            new Sentence("iki piotun kulçandığı uçkata üir hotes görçv alyıor"),
                            new Sentence("son deece kısütlı keilmeler çeçevesinde kendülerini uzuü cümllerle ifüde edbeiliyorlar"),
-                           new Sentence("krdi köpek"),
                            new Sentence("minibü durağı"),
                            new Sentence("ntoer belgesi"),
                            new Sentence("")};
     NGramSpellChecker nGramSpellChecker = NGramSpellChecker(fsm, *nGram, true);
-    for (int i = 0; i < 14; i++){
+    for (int i = 0; i < 13; i++){
         REQUIRE(original[i]->to_string() == nGramSpellChecker.spellCheck(modified[i])->to_string());
     }
 }
